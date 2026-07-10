3388233fe8d92d5f0c2d8609fe7fbc5d8e4d4437
Allzer <dshapkarin@list.ru>
2026-07-10 17:30:41 +0300

    задание 1.1

---
+++ 1_part/1_1_task.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 3
+	fmt.Println(a)
+
+	var b float64 = 1.2
+	fmt.Println(b)
+
+	var c string = "QWEASDZXC"
+	fmt.Println(c)
+
+	var d bool = true
+	fmt.Println(d)
+
+	var e rune = 'A'
+	fmt.Println(e)
+}
