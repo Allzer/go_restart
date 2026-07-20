3c6d996b69bf857ce57e87a5519b4d1ac66de35b
Allzer <dshapkarin@list.ru>
2026-07-20 13:37:48 +0300

    6 таск

--- 2_part/6_task/6_task.go
+++ 2_part/6_task/6_task.go
@@ -2,27 +2,18 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
 func main() {
-	arr := genArr()
-}
-
-func genArr() []int {
-	arr := make([]int, 10)
-
-	for i := range arr {
-		arr[i] = rand.Intn(100)
-	}
-	fmt.Println(arr)
-	return arr
+	arr := []int{10, 10, 12, 12, 14}
+	popDuble(arr)
 }
 
 func popDuble(arr []int) {
-	for i:= 0; i < len(arr)-1; i++ {
-		if arr[i] == arr[i+1]{
-			fmt.Println(arr[i])
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i] == arr[i+1] {
+			arr = append(arr[:i], arr[i+1:]...)
 		}
 	}
+	fmt.Println(arr)
 }
