2fe3df6f855cbb412fdb138a5387eb8bd0bf119d
Allzer <dshapkarin@list.ru>
2026-07-20 15:43:36 +0300

    15 таск

--- 2_part/15_task/15_task.go
+++ 2_part/15_task/15_task.go
@@ -1,9 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	elementCounter([]int{1, 2, 3, 3, 3, 2, 2, 3, 3})
 }
 
 func elementCounter(arr []int) {
-	
+	counter := make(map[int]int)
+
+	for _, v := range arr {
+		counter[v]++
+	}
+
+	fmt.Println(counter)
 }
