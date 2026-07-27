1704cdbaa829721afd12722be8ac095403aeb89e
Allzer <dshapkarin@list.ru>
2026-07-27 18:40:46 +0300

    перенёс всё в методы и убрал комментарии

--- 5_part/internal/User/userMethods.go
+++ 5_part/internal/User/userMethods.go
@@ -28,18 +28,6 @@ func (user *User) GetOrder() {
 	fmt.Printf("Заказ номер %d на сумму %.2f получил статус %s", user.Orders.ID, user.Orders.TotalPrice, user.Orders.Status)
 }
 
-// func (user *User) AddBaseOrder() {
-// 	user.Orders = order.CreateBaseOrder(user.Orders)
-// }
-
-// func (user *User) СhooseProduct() {
-// 	user.Orders = order.ChooseProduct(user.Orders)
-// }
-
-// func (user *User) CompleteOrder() {
-// 	user.Orders = order.CompleteOrder(user.Orders)
-// }
-
 func (user *User) CreateNewOrder() {
 	user.Orders = order.Order{}
 }
